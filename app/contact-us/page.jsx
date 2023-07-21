d699de3733f2fe4b3f56648e3dcae1594a371871
Safet Duranovic <duranovic.safet@gmail.com>
2023-07-21 06:03:27 -0400

    Check if mounted with useeffect and dynamically import map component

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,10 +1,16 @@
 import '@styles/base/page.scss';
 import Footer from '@layout/Footer/Footer';
 import HeaderSecondary from '@layout/HeaderSecondary/HeaderSecondary';
-import Map from '@layout/Map/Map';
+// import Map from '@layout/Map/Map';
 import FormSection from '@layout/FormSection/FormSection';
+import { useMemo } from 'react';
+import dynamic from 'next/dynamic';
 
 const ContactPage = () => {
+	const Map = useMemo(
+		() => dynamic(() => import('@layout/Map/Map'), { ssr: false }),
+		[]
+	);
 	return (
 		<>
 			<HeaderSecondary />
