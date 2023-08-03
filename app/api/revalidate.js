d646061c6505c310b2f8f4cafc5b02960e8a69ad
Safet Duranovic <duranovic.safet@gmail.com>
2023-08-03 20:01:05 +0200

    Recode revalidation

--- app/api/revalidate.js
+++ app/api/revalidate.js
@@ -1,31 +1,37 @@
-import { isValidRequest, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
+import { isValidRequest } from '@sanity/webhook'
 
 const secret = 'ofwUPCz05sHgM0xq+de5axjTzIlfLtjYWq9x081O'
 
+import { isValidRequest } from "@sanity/webhook"
+
+
+
+
+
 export default async function handler(req, res) {
-    const signature = request.headers[SIGNATURE_HEADER_NAME]
-    const body = await readBody(req) // Read the body into a string
-    if (!isValidSignature(body, signature, secret)) {
-        res.status(401).json({ success: false, message: 'Invalid signature' })
+    if (req.method !== "POST") {
+        console.error("Must be a POST request")
+        return res.status(401).json({ message: "Must be a POST request" })
+    }
+
+    if (!isValidRequest(req, secret)) {
+        res.status(401).json({ message: "Invalid signature" })
         return
     }
 
-    const jsonBody = JSON.parse(body)
-    doSomeMagicWithPayload(jsonBody)
-    res.json({ success: true })
-}
+    try {
+        const {
+            body: { type, slug },
+        } = req
 
-// Next.js will by default parse the body, which can lead to invalid signatures
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-}
+        switch (type) {
+            case "post":
+                await res.revalidate(`/news/${slug}`)
+                return res.json({ message: `Revalidated "${type}" with slug "${slug}"` })
+        }
 
-async function readBody(readable) {
-    const chunks = []
-    for await (const chunk of readable) {
-        chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+        return res.json({ message: "No managed type" })
+    } catch (err) {
+        return res.status(500).send({ message: "Error revalidating" })
     }
-    return Buffer.concat(chunks).toString('utf8')
 }
